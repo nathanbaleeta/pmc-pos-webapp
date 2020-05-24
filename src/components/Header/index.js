cbd9834b47258b677f7631c58be0f4660931ecfd
nathanbaleeta <nbaleeta@gmail.com>
2020-05-24 15:00:49 +0300

    Added responsive layout css trick using zoom to header component for uniformity across desktop/laptop browsers

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -22,7 +22,7 @@ import MailIcon from "@material-ui/icons/Mail";
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
-    zoom: "75%",
+    zoom: "80%",
   },
   menuButton: {
     marginRight: theme.spacing(2),
